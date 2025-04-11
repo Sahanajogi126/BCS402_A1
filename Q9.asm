d5f9e8ddd2e0f55f167c18d770dc45cb42eb5cb7
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-11 20:16:46 +0530

    Q9.asm

--- Q9.asm
+++ Q9.asm
@@ -3,13 +3,12 @@ ENTRY
 
        
     LDR  r2, =0x40000000
-	LDR  r3, =0x12341234
+    LDR  r3, =0x12341234
     STR  r3, [r2]                
     LDR  r1, =0x11112222          
     MOV  r0, #0x0
 
-      
-        SWP     r0, r1, [r2]
+    SWP     r0, r1, [r2]
 
      
      END
